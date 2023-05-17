0448c22cbd596a4bcc68fb3691f064297683dea3
prakharsaxena3007 <prakhar.saxena@fiftyfivetech.io>
2023-05-17 13:12:10 +0530

    First Push

---
+++ src/main/java/com/example/usermanagement/UserManagementApplication.java
@@ -0,0 +1,19 @@
+package com.example.usermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+
+public class UserManagementApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManagementApplication.class, args);
+	}
+
+
+}
