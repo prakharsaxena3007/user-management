b8de0807106445d4225d27e97b507c3fb587622a
prakharsaxena3007 <prakhar.saxena@fiftyfivetech.io>
2023-05-31 15:12:31 +0530

    Imports that was missing

--- src/main/java/com/example/usermanagement/controller/UserController.java
+++ src/main/java/com/example/usermanagement/controller/UserController.java
@@ -1,8 +1,8 @@
 package com.example.usermanagement.controller;
 
-import com.example.usermanagement.dto.*;
 import com.example.usermanagement.dto.Response;
 import com.example.usermanagement.dto.UpdateUserDto;
+import com.example.usermanagement.dto.UserPasswordDto;
 import com.example.usermanagement.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
