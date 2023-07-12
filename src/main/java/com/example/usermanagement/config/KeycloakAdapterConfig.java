aba758a36ffd290d7b32175a52d1d9e1e1a628aa
prakharsaxena3007 <prakhar.saxena@fiftyfivetech.io>
2023-07-12 15:04:39 +0530

    SpringApp Containerisation

--- src/main/java/com/example/usermanagement/config/KeycloakAdapterConfig.java
+++ src/main/java/com/example/usermanagement/config/KeycloakAdapterConfig.java
@@ -1,9 +1,11 @@
 package com.example.usermanagement.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = {"com.example.usermanagement"})
 public class KeycloakAdapterConfig {
 
     @Value("${keycloak.auth-server-url}")
